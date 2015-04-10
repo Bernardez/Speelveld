fefd751b046d1dad82ee4cf72b1c6557656bf707
Bernardez <bas_siegerink@hotmail.com>
2015-04-10 10:46:02 +0200

    compatability (ofzo)

--- src/nl/noscope/emeraldextraction/EmeraldExtraction.java
+++ src/nl/noscope/emeraldextraction/EmeraldExtraction.java
@@ -22,7 +22,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
